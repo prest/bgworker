8d23269ab4cd8dd09179e202e8972cf42f31dce5
Fabrízio de Royes Mello <fabriziomello@gmail.com>
2017-09-19 16:25:43 -0300

    Move background worker main loop from C to Go code

--- main.go
+++ main.go
@@ -1,15 +1,35 @@
 package main
 
 /*
-extern void BackgroundWorkerMain();
-void elog_log(char *string);
+#include "main_c.h"
 */
 import "C"
 
 func main() {}
 
 //export BackgroundWorkerMain
-func BackgroundWorkerMain(){
-	C.elog_log(C.CString("Hello World from GoLang!!! Yeahhhhh!!!!"));
+func BackgroundWorkerMain() C.int {
+
+	C.elog_log(C.CString("Starting GoBackgroundWorker"))
+
+	for C.get_got_sigterm() == 0 {
+		var rc C.int
+
+		/* Wait 10s */
+		rc = C.wait_latch(C.long(10000))
+
+		C.reset_latch()
+
+		/* Emergency bailout if postmaster has died */
+		if C.postmaster_is_dead(rc) != 0 {
+			C.elog_log(C.CString("Aborting GoBackgroundWorker"))
+			return 1
+		}
+
+		C.elog_log(C.CString("Hello World from GoLang!!! Yeahhhhh!!!!"));
+	}
+
+	C.elog_log(C.CString("Finishing GoBackgroundWorker"))
+	return 0
 }
 
